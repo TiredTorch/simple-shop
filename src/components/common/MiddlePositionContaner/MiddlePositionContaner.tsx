48f924b0e1c30be9c73ed19f6acc0b05cfa8f17d
Viktor Ivanov <vitochek337@gmail.com>
2024-07-18 15:18:23 +0300

    Configured responsive for profile page

--- src/components/common/MiddlePositionContaner/MiddlePositionContaner.tsx
+++ src/components/common/MiddlePositionContaner/MiddlePositionContaner.tsx
@@ -2,7 +2,7 @@ import { FC, PropsWithChildren } from "react";
 
 export const MiddlePositionContaner: FC<PropsWithChildren> = ({ children }) => {
     return (
-        <div className="h-[calc(100dvh-6rem)] bg-slate-400 w-2/3 m-auto px-4 py-6 flex flex-col items-center justify-between overflow-auto">
+        <div className="h-[calc(100dvh-6rem)] bg-slate-400 sm:w-2/3 w-[100dvw] max-w-[100dvw] m-auto px-4 py-6 flex flex-col items-center justify-between overflow-y-auto overflow-x-hidden">
             {children}
         </div>
     );
--- src/containers/ProfileContainer/ProfileContainer.tsx
+++ src/containers/ProfileContainer/ProfileContainer.tsx
@@ -1,47 +1,29 @@
-"use client";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { useCallback } from "react";
+import Link from "next/link";
 import { Button, MiddlePositionContaner } from "@/components";
 
 export const ProfileContainer = () => {
-    const router = useRouter();
-
-    const handleNavigateToCart = useCallback(() => {
-        router.push("/en/cart");
-    }, [router]);
-
-    const handleNavigateToOrders = useCallback(() => {
-        router.push("/en/status");
-    }, [router]);
-
     return (
         <MiddlePositionContaner>
-            <div className="flex w-full">
+            <div className="flex w-full sm:flex-row flex-col items-center gap-4">
                 <Image alt="avatar" src={""} className="w-3/5" />
                 <div className="flex flex-col items-center w-2/5 gap-4">
-                    <div className="text-4xl text-cyan-700 font-bold">
+                    <div className="sm:text-4xl text-2xl text-cyan-700 font-bold">
                         Username
                     </div>
-                    <div className="text-2xl text-cyan-700 font-semibold">
+                    <div className="sm:text-2xl text-lg text-cyan-700 font-semibold">
                         Purchases:&nbsp;
                         <span className="text-cyan-800">2</span>
                     </div>
                 </div>
             </div>
             <div className="flex justify-evenly w-full">
-                <Button
-                    buttonVariation="profileButton"
-                    onClick={handleNavigateToCart}
-                >
-                    Cart
-                </Button>
-                <Button
-                    buttonVariation="profileButton"
-                    onClick={handleNavigateToOrders}
-                >
-                    Orders
-                </Button>
+                <Link href={"/en/cart"}>
+                    <Button buttonVariation="profileButton">Cart</Button>
+                </Link>
+                <Link href={"/en/status"}>
+                    <Button buttonVariation="profileButton">Orders</Button>
+                </Link>
             </div>
         </MiddlePositionContaner>
     );
